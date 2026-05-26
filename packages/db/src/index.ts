644022bb12a66bb0cc0654ab920a1941771941d7
mylife-as-miles <mylife-as-miles@users.noreply.github.com>
2026-05-26 06:51:25 +0100

    feat(db): export database package entrypoint

---
+++ packages/db/src/index.ts
@@ -0,0 +1,3 @@
+export { initPool, getPool, closePool } from "./postgres.js";
+export { PostgresAdapter } from "./postgresAdapter.js";
+export { encryptConfigJson, decryptConfigJson, encryptValue, decryptValue, maskedApiKey } from "./crypto.js";
